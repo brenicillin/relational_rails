e3bc497aec17da6e3391475513caf0a8633a3e6b
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-03-26 14:56:24 -0400

    update routes and add views for new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/dealerships', to: 'dealerships#index'
-  get '/dealerships/:id', to: 'dealerships#show'
+  get '/', to: 'welcome#index'
   get '/cars', to: 'cars#index'
   get '/cars/:id', to: 'cars#show'
+
+  get '/dealerships', to: 'dealerships#index'
+  get '/dealerships/new', to: 'dealerships#new'
+  get '/dealerships/:id', to: 'dealerships#show'
   get '/dealerships/:id/cars', to: 'dealerships#show_cars'
+
+  post '/dealerships', to: 'dealerships#create'
 end
